eafb4e2525a4f6837560bac81af06b220582d563
Emili Sophia Cordeiro <emilisophiassss@gmail.com>
2024-11-15 17:34:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function comprarBitcoin() {
     quantidade = Number(quantidade)
     if (quantidade > 0) {
         saldoBitcoin += quantidade
-        alert(`Você comprou ${quantidade} bitcoins. Seu saldo atual é de ${saldoBitcoin} bitcoins.`)
+        alert(`Você comprou ${quantidade} bitcoins. Seu saldo atual é de ${saldoBitcoin} bitcoins. O valor em reais foi de R$${quantidade * 523811}.`)
     } else {
         alert("Você não pode comprar bitcoins negativos.")
     }
@@ -24,7 +24,7 @@ function venderBitcoin() {
     if (quantidade > 0) {
         if (quantidade <= saldoBitcoin) {
             saldoBitcoin -= quantidade
-            alert(`Você vendeu ${quantidade} bitcoins. Seu saldo atual é de ${saldoBitcoin} bitcoins.`)
+            alert(`Você vendeu ${quantidade} bitcoins. Seu saldo atual é de ${saldoBitcoin} bitcoins. O valor em reais foi de R$${quantidade * 523811}.`)
         } else {
             alert("Você não possui essa quantidade de bitcoins para vender.")
         }
@@ -33,7 +33,7 @@ function venderBitcoin() {
     //loop
  while (true) {
     //menu
-switch (prompt("O que você deseja fazer? \n1 - Comprar bitcoins \n2 - Vender bitcoins \n3 - Ver saldo \n4 - Sair")) {
+switch (prompt("O que você deseja fazer? \n1 - Comprar bitcoins \n2 - Vender bitcoins \n3 - Ver saldo \n4 - Como funciona o Bitcoin \n5 - Quanto custa o Bitcoin \n6 - Sair")) {
     case "1": //comprar
         comprarBitcoin()
         break
@@ -41,12 +41,18 @@ switch (prompt("O que você deseja fazer? \n1 - Comprar bitcoins \n2 - Vender b
         venderBitcoin()
         break
     case "3": //ver saldo
-        alert(`Seu saldo atual é de ${saldoBitcoin} bitcoins.`)
+        alert(`Seu saldo atual é de ${saldoBitcoin} bitcoins. O valor em reais foi de R$${saldoBitcoin * 523811}.`)
         break
-    case "4": //sair
+    case "4": //como funciona o bitcoin
+    alert(" Bitcoin é uma criptomoeda digital que funciona como uma moeda de troca virtual. \nO bitcoin foi criado em 2009 por Satoshi Nakamoto, um programador americano. \nVocê pode comprar bitcoins usando seu saldo em reais, e vender bitcoins usando seu saldo em bitcoins. \nAqui estão algumas das principais características do Bitcoin:	 \nTransações: As pessoas podem enviar e receber Bitcoins entre si. Essas transações são agrupadas em blocos. \nBlockchain: A blockchain é um livro-razão público e descentralizado onde todas as transações de Bitcoin são registradas. Cada bloco contém um conjunto de transações e é ligado ao bloco anterior, formando uma cadeia. \nMineração: Os mineradores são indivíduos ou organizações que utilizam computadores poderosos para resolver complexos problemas matemáticos. Esse processo de mineração confirma e verifica as transações de Bitcoin, adicionando novos blocos à blockchain. Em troca, os mineradores são recompensados com novos Bitcoins. \nDescentralização: Não há uma autoridade central controlando o Bitcoin. A rede é mantida por uma vasta rede de computadores ao redor do mundo, tornando-a resistente à censura e ao controle centralizado. \nCarteira Digital: Para armazenar e utilizar Bitcoins, você precisa de uma carteira digital, que pode ser um software em seu computador ou smartphone. \n")
+        break
+        case "5"://quanto custa o bitcoin
+        alert("Atualmente, o valor de 1 Bitcoin (BTC) \né aproximadamente R$ 523,811,00. \nVale lembrar que o preço do Bitcoin pode \nvariar bastante devido à alta volatilidade \ndo mercado de criptomoedas.")        
+        break
+        case "6": //sair
         alert("Obrigado por usar o gerenciador de bitcoins!")
         break
-    default:
+        default:
 }}
 
 
